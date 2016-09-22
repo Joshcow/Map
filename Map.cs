9d66e2828413fbf05ca8b87e657d51d4d6f859cd
Joshcow <joshcrip@hotmail.com>
2016-09-22 12:45:23 -0400

    Add files via upload

---
+++ Map.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+	public GameObject hexPrefab;
+
+	int width = 20;
+	int height = 20;
+
+	float xOffset = 0.884f;
+	float zOffset = 0.764f;
+
+
+
+	// Use this for initialization
+	void Start () { 
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+
+				float xPos = x * xOffset;
+
+				if (y % 2 == 1) {
+					xPos += xOffset/2f;
+
+				}
+
+				Instantiate (hexPrefab, new Vector3( xPos,0,y * zOffset  ), Quaternion.identity  );
+			}
+		}
+			
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update (){ 
+	
+	}
+}
